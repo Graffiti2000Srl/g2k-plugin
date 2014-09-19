28756a0bbf1128be1d057bbe7e39a1e1b683cbde
Zweer <flicofloc@gmail.com>
2014-09-19 16:42:09 +0200

    Changed the priority of the admin bar node

--- g2k-admin-bar.php
+++ g2k-admin-bar.php
@@ -3,9 +3,11 @@
 require_once __DIR__ . '/g2k-module.php';
 
 abstract class G2K_Admin_Bar extends G2K_Module {
+	protected $_priority = 100;
+
 	protected function _register_hooks() {
-		add_action('admin_bar_menu', array($this, 'create_admin_bar'));
+		add_action('admin_bar_menu', array($this, 'create_admin_bar'), $this->_priority);
 	}
 
-	abstract public function create_admin_bar();
+	abstract public function create_admin_bar(WP_Admin_Bar $wp_admin_bar);
 }
