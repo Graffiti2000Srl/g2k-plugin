6c54a8d99b26e17bea0063b4cfeff8044a8f826b
Zweer <flicofloc@gmail.com>
2014-09-19 11:53:24 +0200

    Added comments and edited the require_once logic

--- g2k-plugin.php
+++ g2k-plugin.php
@@ -8,10 +8,20 @@ abstract class G2K_Plugin extends G2K_Singleton {
 	const VERSION = '';
 	const REQUIRED_CAPABILITY = '';
 
+	/**
+	 * Activates the plugin.
+	 *
+	 * It's only a base method, so it only flushes the rewrite rules.
+	 */
 	public function activate() {
 		flush_rewrite_rules();
 	}
 
+	/**
+	 * Deactivates the plugin.
+	 *
+	 * It's only a base method, so it only flushes the rewrite rules.
+	 */
 	public function deactivate() {
 		flush_rewrite_rules();
 	}
@@ -24,14 +34,12 @@ abstract class G2K_Plugin extends G2K_Singleton {
 	 * in sub-directories to avoid cluttering the root folder. In both cases, the theme/plugin will have access to the variables so they can
 	 * fully customize the output.
 	 *
-	 * @mvc @model
-	 *
 	 * @param  string $default_template_path The path to the template, relative to the plugin's `views` folder
 	 * @param  array  $variables             An array of variables to pass into the template's scope, indexed with the variable name so that it can be extract()-ed
-	 * @param  string $require               'once' to use require_once() | 'always' to use require()
+	 * @param  bool   $require_once          'once' to use require_once() | 'always' to use require()
 	 * @return string
 	 */
-	protected static function _render_template( $default_template_path = null, $variables = array(), $require = 'once' ) {
+	protected static function _render_template( $default_template_path = null, $variables = array(), $require_once = true ) {
 		do_action( static::PREFIX . '_render_template_pre', $default_template_path, $variables );
 
 		$template_path = locate_template( basename( $default_template_path ) );
@@ -44,7 +52,7 @@ abstract class G2K_Plugin extends G2K_Singleton {
 			extract( $variables );
 			ob_start();
 
-			if ( 'always' == $require ) {
+			if ( !$require_once ) {
 				require( $template_path );
 			} else {
 				require_once( $template_path );
