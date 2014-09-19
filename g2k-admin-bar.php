de06a4f3adf9498e5ce63a67fa5febcdb651783f
Zweer <flicofloc@gmail.com>
2014-09-19 15:15:18 +0200

    Added the hooks

--- g2k-admin-bar.php
+++ g2k-admin-bar.php
@@ -3,5 +3,9 @@
 require_once __DIR__ . '/g2k-module.php';
 
 abstract class G2K_Admin_Bar extends G2K_Module {
+	protected function _register_hooks() {
+		add_action('admin_bar_menu', array($this, 'create_admin_bar'));
+	}
 
+	abstract public function create_admin_bar();
 }
